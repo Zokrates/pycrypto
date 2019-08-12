20ff03ff69349d00b8f05dda27d262a0c23a919e
Wanseob Lim <email@wanseob.com>
2019-08-12 15:06:29 +0900

    Fix inv() for FQ
    closes #4

--- zokrates_pycrypto/field.py
+++ zokrates_pycrypto/field.py
@@ -31,7 +31,8 @@ def inv(a: int, n: int) -> int:
     if a == 0:
         return 0
     lm, hm = 1, 0
-    low, high = a % n, n
+    num = a if isinstance(a, int) else a.n
+    low, high = num % n, n
     while low > 1:
         r = high // low
         nm, new = hm - lm * r, high - low * r
