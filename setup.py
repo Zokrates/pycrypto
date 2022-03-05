aa8a98b20109e1929aa41f6dd2e60c19ebafdda8
Leonard Stutzer <leonard.stutzer@campus.tu-berlin.de>
2022-03-05 10:16:34 +0100

    add find_packages again for generic approach

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='zokrates_pycrypto',
       version='1.0',
       install_requires=[
           'bitstring==3.1.5'
       ],
-      packages=['zokrates_pycrypto', 'zokrates_pycrypto.gadgets']
+      packages=find_packages()
      )
