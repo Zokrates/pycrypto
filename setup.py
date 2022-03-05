cd4698c8c0b0f6b218698a4c8667e9140533d2b5
Leonard Stutzer <leonard.stutzer@campus.tu-berlin.de>
2022-03-05 10:14:07 +0100

    fix setup.py to install pycrypto correctly

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='zokrates_pycrypto',
       version='1.0',
-      packages=find_packages(),
+      install_requires=[
+          'bitstring==3.1.5'
+      ],
+      packages=['zokrates_pycrypto', 'zokrates_pycrypto.gadgets']
      )
